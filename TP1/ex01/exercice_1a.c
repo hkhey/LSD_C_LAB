b19836c86b1c39073160a7f5b66071bd2250c3bf
hkhey <76151779+hkhey@users.noreply.github.com>
2020-12-20 10:17:18 +0100

    Update exercice_1a.c

--- TP1/ex01/exercice_1a.c
+++ TP1/ex01/exercice_1a.c
@@ -1,59 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
+
+#include<stdio.h>
+#include<stdlib.h>
 #define N 10
-
-
-float *descente(float A[N][N], float B[N], int n)
+float *descente(float A[N][N],float B[N],int n)
 {
-  float   *x;
-
-  /* Here we allocate the result verctor \
-  (you're gonna do it yourself in the next exercises)*/
-  x = malloc(sizeof(float) * n);
-
-
-  /******Implement the solution here******/
-  /***************************************/
-  
-  /****************************************/
-
-  return (x);
+ float *X;
+ X=malloc(sizeof(float)*n);
+ X[0]=B[0]/A[0][0];
+ for(int i=1;i<n;i++)
+ {
+ float s=0;
+ for(int j=0;j<i;j++)
+ {
+  s+=A[i][j]*X[j];
+ }
+ X[i]=(B[i]-s)/A[i][i];
+ }
+ return(X);
 }
-
 
 int main()
 {
-  float   A[N][N], B[N];
-  float   *x;
-  int     n;
-
-  printf("Enter the size of the matrix: ");
-  scanf("%d", &n);
-
-  /* Filling the matrix A */
-  printf("Filling the matrix A\n");
-  for(int i = 0; i < n; i++)
-  {
-    for(int j = 0; j < n; j++)
-    {
-      printf("A[%d][%d] = ", i, j);
-      scanf("%f", &A[i][j]);
-    }
-  }
-
-  /* Filling the vector B*/
-  printf("Filling the matrix B\n");
-  for (int i = 0; i < n; i++)
-  {
-    printf("B[%d] = ", i);
-    scanf("%f", &B[i]);
-  }
-
-  /* The calculation of the result */
-  x = descente(A, B, n);
-  
-  /* Printing the results */
-  printf("\nThe resulting vector: [");
-  for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+ float A[N][N],B[N];
+ float *x;
+ int n;
+ printf("Enter the size of the matrix:");
+ scanf("%d",&n);
+ printf("Filling the matrix:\n");
+ for(int i=0;i<n;i++)
+ {
+ for(int j=0;j<n;j++)
+ {
+ printf("A[%d][%d] = ",i,j);
+ scanf("%f",&A[i][j]);
+ }
+ }
+ printf("Filling the matrix B\n");
+ for(int i=0;i<n;i++)
+ {
+ printf("B[%d] = ",i);
+ scanf("%f",&B[i]);
+ }
+ x=descente(A,B,n);
+ printf("\n The resulting vector :[");
+ for(int i=0;i<n;i++)
+ printf("%f%c",x[i], ",]"[i==n-1]);
 }
